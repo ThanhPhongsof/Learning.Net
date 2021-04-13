d6ab02dd7edf2d8a1ace8dd545bd3ed310c3b382
ThanhPhongsof <ttphongletter@gmail.com>
2021-04-13 17:06:36 +0700

    add RabbitMQ

---
+++ RabbitMQ/ReceiveConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiveConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RabbitMQ/SendConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace SendConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var con = factory.CreateConnection())
+            {
+                using (var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "");
+                }
+            }
+        }
+    }
+}
